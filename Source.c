d97154e910b020f282ac9ed5b3b917d45607ed5b
1337xp <1337xp101@gmail.com>
2021-04-15 18:25:35 +0200

    Create Source.c

---
+++ Source.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int input1;
+	int input2;
+	input1 = 10;
+	input2 = 10;
+
+	printf("%d\n", input1 + input2);
+	printf("%d\n", input1 / input2);
+	printf("%d\n", input1 * input2);
+	printf("%d\n", input1 - input2);
+	printf("%d\n", input1 % input2);
+
+}
